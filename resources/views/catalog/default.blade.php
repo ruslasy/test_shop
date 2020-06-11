cc592bf8a8b5c9f07316020ca5241509e26308b5
ruslasy <ruslasy@yandex.ru>
2020-06-11 20:03:22 +0300

    views product card

--- resources/views/catalog/default.blade.php
+++ resources/views/catalog/default.blade.php
@@ -3,8 +3,26 @@
 @section('title', 'Catalog')
 
 @section('content')
-    <h1>hello world!!!!!</h1>
+    
+<div class="row row-cols-1 row-cols-md-3">
     @foreach ($products as $product)
-        <p>{{ $product->name }}</p>
-    @endforeach
+    <div class="col mb-4">
+        <div class="card mb-4 box-shadow">
+          <div class="card-body">
+            <img src="https://www.claires.com/dw/image/v2/BBTK_PRD/on/demandware.static/-/Sites-master-catalog/default/dw91a8534a/images/hi-res/35328_1.jpg" alt="..." class="card-img-top">
+            <hr>
+            <div class="row no-gutters">
+                <div class="col-md-6">
+                    <h5 class="card-title text-sm-left">{{ $product->price }} руб.</h5>
+                </div>
+                <div class="col-md-6">
+                    <button type="button" class="btn btn-lg btn-block btn-outline-primary">в корзину</button>
+                </div>
+            </div>
+          </div>
+          </div>
+        </div>
+          @endforeach
+        </div>
 @endsection
+
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,13 +2,68 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+        <script src="{{ mix('/js/app.js') }}"></script>
+        <link href="https://bootstrap-4.ru/dist/css/bootstrap.min.css"  rel="stylesheet">
+        <link href="{{ mix('/css/app.css') }}"  rel="stylesheet">
+
         <title>App Name - @yield('title')</title>
     </head>
     <body>
+    <div class="d-flex flex-column align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
+      <nav class="my-0 my-md-auto mr-md-3">
+        <a class="p-2 text-dark" href="/">Главная</a>
+        <a class="p-2 text-dark" href="/catalog">Каталог</a>
+      </nav>
+    </div>
 
+    <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+      <h1 class="display-4">Каталог товаров</h1>
+    </div>
         <div class="container">
             @yield('content')
         </div>
+
+      <footer class="pt-4 my-md-5 pt-md-5 border-top">
+        <div class="row">
+          <div class="col-12 col-md">
+            <img class="mb-2" src="/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="24" height="24">
+            <small class="d-block mb-3 text-muted">&copy; 2017-2018</small>
+          </div>
+          <div class="col-6 col-md">
+            <h5>Features</h5>
+            <ul class="list-unstyled text-small">
+              <li><a class="text-muted" href="#">Cool stuff</a></li>
+              <li><a class="text-muted" href="#">Random feature</a></li>
+              <li><a class="text-muted" href="#">Team feature</a></li>
+              <li><a class="text-muted" href="#">Stuff for developers</a></li>
+              <li><a class="text-muted" href="#">Another one</a></li>
+              <li><a class="text-muted" href="#">Last time</a></li>
+            </ul>
+          </div>
+          <div class="col-6 col-md">
+            <h5>Resources</h5>
+            <ul class="list-unstyled text-small">
+              <li><a class="text-muted" href="#">Resource</a></li>
+              <li><a class="text-muted" href="#">Resource name</a></li>
+              <li><a class="text-muted" href="#">Another resource</a></li>
+              <li><a class="text-muted" href="#">Final resource</a></li>
+            </ul>
+          </div>
+          <div class="col-6 col-md">
+            <h5>About</h5>
+            <ul class="list-unstyled text-small">
+              <li><a class="text-muted" href="#">Team</a></li>
+              <li><a class="text-muted" href="#">Locations</a></li>
+              <li><a class="text-muted" href="#">Privacy</a></li>
+              <li><a class="text-muted" href="#">Terms</a></li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </div>
+
     </body>
 </html>
