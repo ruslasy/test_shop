be817bf894b97972cc9fa18ee01ac2ad036d453f
ruslasy <ruslasy@yandex.ru>
2020-06-12 23:12:24 +0300

    fix front

--- resources/js/cart.js
+++ resources/js/cart.js
@@ -1,12 +1,9 @@
+let cartCount = document.getElementById('cart-product-count');
 
 document.querySelectorAll('.add-cart-action').forEach(el => {
     el.addEventListener("click", addToCart, false);
 });
-/*
-document.querySelectorAll('.add-cart-action-del').forEach(el => {
-  el.addEventListener("click", DelFromCart, false);
-});
-*/
+
 function addToCart(){
     let idProduct = this.getAttribute('data-id-product');
 
@@ -14,7 +11,6 @@ function addToCart(){
         return response.json();
       })
       .then((data) => {
-        let cartCount = document.getElementById('cart-product-count');
         cartCount.innerHTML = data['count'];
         if (this.innerHTML == 'в корзину'){
           this.innerHTML = 'удалить';
@@ -28,7 +24,5 @@ fetch('\\catalog/cart/').then((response) => {
   return response.json();
 })
 .then((data) => {
-  let cartCount = document.getElementById('cart-product-count');
   cartCount.innerHTML = data['count'];
-  document.getElementById('cart-product-count').style.display = 'flex';
 });
